cb0b4e239bb4392ddd1ef12601cfff51a2d171c0
Harshit <harshit12063@gmail.com>
2026-07-29 18:30:04 +0530

    Added Service and Controller package

--- src/main/java/com/harsh/urlShortener/controller/HelloController.java
+++ src/main/java/com/harsh/urlShortener/controller/HelloController.java
@@ -1,12 +1,20 @@
 package com.harsh.urlShortener.controller;
 
+import com.harsh.urlShortener.service.HelloService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService){
+        this.helloService = helloService;
+    }
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello, this is spring application!";
+        return helloService.sayHello();
     }
 }
